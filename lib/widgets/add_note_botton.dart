7f25215ee8de071d9685b3a3ee729ac22186ef82
m07amadd <farrajeenmohammed@gmail.com>
2024-07-28 02:27:42 +0300

    add field to botton sheet

--- lib/widgets/add_note_botton.dart
+++ lib/widgets/add_note_botton.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/widgets/custom_text_field.dart';
 
 class AddNoteButton extends StatelessWidget {
   const AddNoteButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 35,
+          ),
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+
+  final String hint;
+  final int maxLines;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: hint,
+      ),
+    );
+  }
+}
