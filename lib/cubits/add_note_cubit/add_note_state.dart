44513c7472bb00c46bf79bfba092fc1afc810751
m07amadd <farrajeenmohammed@gmail.com>
2024-09-06 20:49:07 +0300

    add add notes state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,13 @@ import 'package:flutter/widgets.dart';
 abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesSuccess extends NotesState {}
+
+class NotesFailure extends NotesState {
+  final String errMessage;
+
+  NotesFailure({required this.errMessage});
+}
